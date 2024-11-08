be4f47119f8dae4b232b3c37f27b642d874b8c4c
Mohammad Mostafa <ezmmd83@gmail.com>
2024-11-08 23:40:50 +0330

    updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 		applicationId = "ir.mmd.androidDev.lowheanvar"
 		minSdk = 24
 		targetSdk = 35
-		versionCode = 3
-		versionName = "0.3.0"
+		versionCode = 4
+		versionName = "0.4.0"
 		
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
